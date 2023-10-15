1dbff17d3e2cfde4ea4da16d55b1cc576c3b1bdf
gustavo-atanazio <gus.atanazio@gmail.com>
2023-10-15 16:21:36 -0300

    Interface de interação do usuário

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { BiSearch } from 'react-icons/bi';
+import './App.css';
+
 function App() {
 	return (
-		<div>
-			Weather
+		<div className='main-container'>
+			<div className='container'>
+				<div className='user-input'>
+					<h2>Busque o clima de algum lugar</h2>
+					<div className='user-search'>
+						<input type="text" required/>
+						<button>
+							<BiSearch size={25} color={'#FFF'}/>
+						</button>
+					</div>
+				</div>
+
+				<div className='divisor'></div>
+
+				<div className='main-content'></div>
+			</div>
 		</div>
 	);
 }
