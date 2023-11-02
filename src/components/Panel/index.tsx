24ae5bcd2b0612734bc9d8446e2e76e4a426820d
gustavo-atanazio <gus.atanazio@gmail.com>
2023-11-02 11:37:25 -0300

    Componente Panel

---
+++ src/components/Panel/index.tsx
@@ -0,0 +1,59 @@
+import Lottie from "lottie-react";
+import { TbDropletFilled } from "react-icons/tb";
+
+import Container from "components/Container";
+
+import rain from 'animations/rain.json';
+import styles from './Panel.module.css';
+
+interface PanelProps {
+    size: 'normal' | 'mini'
+    city: string
+    country: string
+    temperature: string
+    humidity?: number
+    sky?: string
+}
+
+function Panel({ size, city, country, temperature, humidity, sky }: PanelProps) {
+    return (
+        <div className={styles.panel}>
+            <Container
+                padding={size === 'mini' ? 0 : ''}
+            >
+                <div
+                    className={styles.content}
+                    style={size === 'mini' ? {flexDirection: 'row'} : {}}
+                >
+                    <div>
+                        <h2>{city}</h2>
+                        <h4>{country}</h4>
+                    </div>
+
+                    <Lottie
+                        animationData={rain}
+                        loop={true}
+                        style={size === 'mini' ? { height: 100 } : { height: 150 }}
+                    />
+
+                    <span
+                        className={styles.temperature}
+                        style={size === 'mini' ? {fontSize: '1.5rem'} : {}}
+                    >
+                        {temperature}
+                    </span>
+
+                    {(humidity && sky) &&
+                        <div className={styles.others}>
+                            <TbDropletFilled size={20} />
+                            <span>{humidity}%</span>
+                            <span>{sky}</span>
+                        </div>
+                    }
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default Panel;
