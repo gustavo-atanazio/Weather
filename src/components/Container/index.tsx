2d3bf4b9282dd25c1b829f51659acb6ac883796e
gustavo-atanazio <gus.atanazio@gmail.com>
2023-11-02 11:37:46 -0300

    Ajustando Container para receber padding

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,12 +1,13 @@
 import styles from './Container.module.css';
 
 interface ContainerProps {
+    padding?: string | number
     children: React.ReactNode
 }
 
-function Container({ children }: ContainerProps) {
+function Container({ padding, children }: ContainerProps) {
     return (
-        <div className={styles.container}>
+        <div className={styles.container} style={{ padding: padding }}>
             {children}
         </div>
     )
