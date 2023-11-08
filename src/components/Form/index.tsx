34b2032eea2a86d2f8d77e2d08c86764edf47661
gustavo-atanazio <gus.atanazio@gmail.com>
2023-11-08 11:24:22 -0300

    Controlando formulário

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,18 +1,32 @@
+import { useMemo, useState } from "react";
 import { BiSearch } from "react-icons/bi";
-import styles from './Form.module.css';
 import Container from "components/Container";
+import styles from './Form.module.css';
 
 function Form() {
+    const [search, setSearch] = useState('');
+    const searchIcon = useMemo(() => <BiSearch size={25} color={'#FFF'}/>, []);
+
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        console.log('pesquisa feita');
+    }
+
     return (
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
             <Container>
                 <h2>Busque o clima de algum lugar</h2>
 
                 <div className={styles.user_search}>
-                    <input type="text" required />
+                    <input
+                        value={search}
+                        onChange={event => setSearch(event.target.value)}
+                        type="text"
+                        required
+                    />
 
                     <button>
-                        <BiSearch size={25} color={'#FFF'} />
+                        {searchIcon}
                     </button>
                 </div>
             </Container>
