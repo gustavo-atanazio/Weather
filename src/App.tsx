4a8ec48f8cabcd584b2e52575bc9b5b710505885
gustavo-atanazio <gus.atanazio@gmail.com>
2023-11-10 11:02:04 -0300

    Construindo requisição

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import Form from 'components/Form';
 import Panel from 'components/Panel';
 import Slider from 'components/Slider/Slider';
 import { Slide, SliderProps } from 'components/Slider';
 
+import WeatherData from 'types/WeatherData';
 import './App.css';
 
 function App() {
+	const [city, setCity] = useState('');
+	const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+
 	const sliderSettings: SliderProps = {
 		autoplay: {
 			delay: 4000,
@@ -18,9 +25,36 @@ function App() {
 		spaceBetween: 15
 	};
 
+	async function getWeatherData(city: string) {
+		try {
+			const response = await axios.get('http://localhost:8000/weather', {
+				params: { city: city }
+			});
+
+			const data: WeatherData = {
+				city: response.data.name,
+				country: response.data.sys.country,
+				temperature: response.data.main.temp,
+				humidity: response.data.main.humidity,
+				sky: response.data.weather[0].description
+			};
+
+			if (data) {
+				setWeatherData(data);
+			}
+		} catch (error) {
+			alert('Ocorreu um erro.');
+			console.log(error);
+		}
+	}
+
+	useEffect(() => {
+		getWeatherData(city);
+	}, [city]);
+
 	return (
 		<main>
-			<Form/>
+			<Form setCity={setCity}/>
 
 			<Panel
 				size='normal'
