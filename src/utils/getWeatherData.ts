7f722e3fdde7b299fd356f86a8bd99c07c129a0b
gustavo-atanazio <gus.atanazio@gmail.com>
2023-11-15 16:43:23 -0300

    Refatorando requisições

--- src/utils/getWeatherData.ts
+++ src/utils/getWeatherData.ts
@@ -1,21 +1,29 @@
 import axios from "axios";
 import WeatherData from "types/WeatherData";
 
-export async function getWeatherData(city: string) {
+export async function getWeatherData(cities: string[]) {
     try {
-        const response = await axios.get('http://localhost:8000/weather', {
-            params: { city: city }
-        });
+        const responses = await Promise.all(
+            cities.map(async city => {
+                const response = await axios.get('http://localhost:8000/weather', {
+                    params: { city: city }
+                });
 
-        const data: WeatherData = {
-            city: response.data.name,
-            country: response.data.sys.country,
-            temperature: response.data.main.temp,
-            humidity: response.data.main.humidity,
-            sky: response.data.weather[0].description
-        };
+                const data: WeatherData = {
+                    city: response.data.name,
+                    country: response.data.sys.country,
+                    temperature: response.data.main.temp,
+                    humidity: response.data.main.humidity,
+                    sky: response.data.weather[0].description,
+                    main: response.data.weather[0].main,
+                    clouds: response.data.clouds.all
+                };
+    
+                return data;
+            })
+        );
 
-        return data;
+        return responses;
     } catch (error) {
         alert('Ocorreu um erro.');
         console.log(error);
